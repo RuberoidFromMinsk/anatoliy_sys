6c7b8b895b8aac289603725f19b500f8bf1917cd
Andrew <39839965+RuberoidFromMinsk@users.noreply.github.com>
2018-12-04 22:11:51 +0300

    Delete XMLrequest.js

--- XMLrequest.js
+++
@@ -1,9 +0,0 @@
-function GetFields(){
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET","request.xml",false);
-    xhr.send();
-    xmlDoc = xhr.responseXML;
-    document.getElementById("name").innerHTML = xmlDoc.getElementsByTagName("name")[0].childNodes[0].nodeValue;
-    document.getElementById("date").innerHTML = xmlDoc.getElementsByTagName("date")[0].childNodes[0].nodeValue;
-    document.getElementById("plases").innerHTML = xmlDoc.getElementsByTagName("plases")[0].childNodes[0].nodeValue;
-}
